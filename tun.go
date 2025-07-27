29feac0048e7a3314d194a8e19b09721f9cbb17c
indakotekno <indakotekno@gmail.com>
2025-07-27 20:01:13 +0700

    Delete tun.go

--- tun.go
+++
@@ -1,9 +0,0 @@
-// === tun.go ===
-// Placeholder for TUN/TAP support implementation
-package main
-
-func SetupTunInterface() error {
-	// Logic to create TUN/TAP interface goes here
-	// Cross-platform note: use gopacket or tuntap package
-	return nil
-}
